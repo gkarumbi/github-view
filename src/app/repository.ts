028aa032338483e055f5c778634252c967ec30f5
gkarumbi <gkarumbi@gmail.com>
2020-06-22 00:08:18 +0300

    Add constructor to create the repository details object

---
+++ src/app/repository.ts
@@ -0,0 +1,5 @@
+export class Repository {
+    constructor(public id:number, public name:string, public full_name:string, public description:string, public created_at:string, public updated_at:string, public homepage:string){
+
+    }
+}
