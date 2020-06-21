6a945dd8350ef090637284f0746c4ce24371e233
gkarumbi <gkarumbi@gmail.com>
2020-06-21 22:09:43 +0300

    Create getGitHubInfo function that return a json object with our basic account information

--- src/app/services/githubconnect.service.ts
+++ src/app/services/githubconnect.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubconnectService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getGitHubInfo(){
+    return this.http.get('https://api.github.com/users/gkarumbi?access_token=f58ee917fa6a74198de3eed284bfb62b89bb47cf');
+  }
 }
